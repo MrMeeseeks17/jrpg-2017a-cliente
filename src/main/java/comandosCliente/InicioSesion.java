a54949987fad566fb8f35102bffe8e152cc6969c
Unknown <jjuancruzrey@gmail.com>
2017-07-08 02:32:28 -0300

    version Estable Segundo Intento

--- src/main/java/comandosCliente/InicioSesion.java
+++ src/main/java/comandosCliente/InicioSesion.java
@@ -10,21 +10,21 @@ public class InicioSesion extends ComandosCliente {
 
 	@Override
 	public void ejecutar() {
-		PaqueteUsuario paqueteUsuario = cliente.getPaqueteUsuario();
-		if (paquete.getMensaje().equals(Paquete.msjExito)) {
-			// El usuario ya inicio sesion
-			paqueteUsuario.setInicioSesion(true);
-
-			// Recibo el paquete personaje con los datos
-			cliente.setPaquetePersonaje((PaquetePersonaje) paquete);
-
-		} else {
-			if (paquete.getMensaje().equals(Paquete.msjFracaso))
-				JOptionPane.showMessageDialog(null, "Error al iniciar sesión.");
-
-			// El usuario no pudo iniciar sesión
-			paqueteUsuario.setInicioSesion(false);
-		}
+//		PaqueteUsuario paqueteUsuario = cliente.getPaqueteUsuario();
+//		if (paquete.getMensaje().equals(Paquete.msjExito)) {
+//			// El usuario ya inicio sesion
+//			paqueteUsuario.setInicioSesion(true);
+//
+//			// Recibo el paquete personaje con los datos
+//			cliente.setPaquetePersonaje((PaquetePersonaje) paquete);
+//
+//		} else {
+//			if (paquete.getMensaje().equals(Paquete.msjFracaso))
+//				JOptionPane.showMessageDialog(null, "Error al iniciar sesión.");
+//
+//			// El usuario no pudo iniciar sesión
+//			paqueteUsuario.setInicioSesion(false);
+//		}
 
 	}
 
--- src/main/java/comandosCliente/Salir.java
+++ src/main/java/comandosCliente/Salir.java
@@ -11,15 +11,16 @@ public class Salir extends ComandosCliente {
 
 	@Override
 	public void ejecutar() {
-		cliente.getPaqueteUsuario().setInicioSesion(false);
-		try {
-			cliente.getSalida().writeObject(new Paquete(Comando.SALIR).getJson());
-			cliente.getSocket().close();
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "Error producido al Salir");
-			System.exit(1);
-		}
+		// cliente.getPaqueteUsuario().setInicioSesion(false);
+		// try {
+		// cliente.getSalida().writeObject(new
+		// Paquete(Comando.SALIR).getJson());
+		// cliente.getSocket().close();
+		// } catch (IOException e) {
+		// JOptionPane.showMessageDialog(null, "Error producido al Salir");
+		// System.exit(1);
+		// }
 
-}
+	}
 
 }
